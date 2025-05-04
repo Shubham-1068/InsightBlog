fe449ecea98925fbc5a33579126ab7d4fc107e00
SHUBHAM <shubhamraj1068@gmail.com>
2025-05-04 09:57:25 +0530

    Feat:added additional routes

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/administrator', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'administrator.html'));
+});
+
+app.get('/done', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'done.html'));
+});
+
+app.get('/flag', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'flag.html'));
+});
+
+app.get('/sitemap', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sitemap.html'));
+});
+
+app.get('/trap', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'trap.html'));
+});
+
+app.get('/testing', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'testing.html'));
+});
+
+app.get('/workshop', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'workshop.html'));
+});
+
+app.get('/zip', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'zip.html'));
+});
+
+app.get('/zipfiles', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'zipfiles.html'));
+});
+
 app.get('/webmaster', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'webmaster.html'));
 });
