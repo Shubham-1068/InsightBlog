531d1638afb9c24f99387f473834b333462274ff
SHUBHAM <shubhamraj1068@gmail.com>
2025-05-09 22:32:05 +0530

    status code

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/assets', (req, res) => {
+  res.sendStatus(404);
+});
+
 app.use(express.static('public'));
 
+// Route handlers
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
